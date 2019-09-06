a95c43f927fe1e7055ce5546f0604dc0957a4868
Michał Łaszczewski <michal@emikse.com>
2019-09-06 13:42:23 +0200

    reactive preFetch

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const ReactiveDaoVue = {
   install(Vue, options) {
     if(!options || !options.dao) throw new Error("dao option required")
     let dao = options.dao
+
     Vue.mixin({
       beforeCreate() {
         if(!this.$options.reactive) return; // Avoid distributed fat
-        const optionData = this.$options.data
+
         if (!this.$options.computed) this.$options.computed = {}
         for(let key in this.$options.reactive) {
           let path = this.$options.reactive[key]
@@ -18,6 +19,7 @@ const ReactiveDaoVue = {
           } else if(path.length !== undefined) {
           } else throw new Error("unknown reactive path "+path)
         }
+        const optionData = this.$options.data
         this.$options.data = function vueReactiveDaoInjectedDataFn () {
           const data = (
               (typeof optionData === 'function')
@@ -45,7 +47,10 @@ const ReactiveDaoVue = {
               reactiveObservables[key].bindErrorProperty(this, key+"Error")
             }
             this.$watch(prefix + key, newPath => {
-              if(reactiveObservables[key]) reactiveObservables[key].unbindProperty(this, key)
+              if(reactiveObservables[key]) {
+                reactiveObservables[key].unbindProperty(this, key)
+                reactiveObservables[key].unbindErrorProperty(this, key+"Error")
+              }
               delete reactiveObservables[key]
               if(newPath) {
                 reactiveObservables[key] = dao.observable(newPath)
@@ -70,6 +75,56 @@ const ReactiveDaoVue = {
         for(let key in reactiveObservables) {
           reactiveObservables[key].unbindProperty(this, key)
           reactiveObservables[key].unbindErrorProperty(this, key+"Error")
+        }
+      }
+    })
+
+    Vue.mixin({
+      beforeCreate() {
+        if(typeof window == 'undefined') return; // NO REACTIVE PREFETCH ON SERVER
+        if(!this.$options.reactivePreFetch) return;
+        if (!this.$options.computed) this.$options.computed = {}
+        this.$options.computed[prefix+"_reactivePreFetch"] = this.$options.reactivePreFetch
+        const optionData = this.$options.data
+        this.$options.data = function vueReactiveDaoInjectedDataFn () {
+          const data = (
+              (typeof optionData === 'function')
+                  ? optionData.call(this)
+                  : optionData
+          ) || {}
+          data.reactivePreFetchedPaths = []
+          data.reactivePreFetchError = null
+          return data
+        }
+      },
+      created() {
+        if(typeof window == 'undefined') return; // NO REACTIVE PREFETCH ON SERVER
+        if(!this.$options.reactivePreFetch) return
+        let paths = this[prefix+"_reactivePreFetch"]
+        if(paths) {
+          this.reactivePreFetchObservable = dao.observable({ paths })
+          this.reactivePreFetchObservable.bindProperty(this, "reactivePreFetchedPaths")
+          this.reactivePreFetchObservable.bindErrorProperty(this, "reactivePreFetchError")
+        }
+        this.$watch(prefix+"_reactivePreFetch", paths => {
+          if(this.reactivePreFetchObservable) {
+            this.reactivePreFetchObservable.unbindProperty(this, "reactivePreFetchedPaths")
+            this.reactivePreFetchObservable.unbindErrorProperty(this, "reactivePreFetchError")
+          }
+          delete this.reactivePreFetchObservable
+          if(paths) {
+            this.reactivePreFetchObservable = dao.observable({ paths })
+            this.reactivePreFetchObservable.bindProperty(this, "reactivePreFetchedPaths")
+            this.reactivePreFetchObservable.bindErrorProperty(this, "reactivePreFetchError")
+          }
+        })
+      },
+      beforeDestroy() {
+        if(typeof window == 'undefined') return; // NO REACTIVE PREFETCH ON SERVER
+        if(!this.$options.reactivePreFetch) return; // Avoid distributed fat
+        if(this.reactivePreFetchObservable) {
+          this.reactivePreFetchObservable.unbindProperty(this, "reactivePreFetchedPaths")
+          this.reactivePreFetchObservable.unbindErrorProperty(this, "reactivePreFetchError")
         }
       }
     })
